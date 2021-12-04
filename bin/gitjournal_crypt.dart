9c412a581ee452d479c5d3232523d4c4ca2f7cba
Vishesh Handa <me@vhanda.in>
2021-12-05 00:02:44 +0100

    Properly implement init

--- bin/gitjournal_crypt.dart
+++ bin/gitjournal_crypt.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
+import 'dart:math';
 
-import 'package:gitjournal_crypt/gitjournal_crypt.dart' as gitjournal_crypt;
+// import 'package:gitjournal_crypt/gitjournal_crypt.dart' as gitjournal_crypt;
 import 'package:dart_git/dart_git.dart';
 
 Future<void> main(List<String> arguments) async {
-  print('Hello world: ${gitjournal_crypt.calculate()}!');
+  // print('Hello world: ${gitjournal_crypt.calculate()}!');
 
   if (arguments.isEmpty) {
     print("Arguments empty");
@@ -13,10 +14,12 @@ Future<void> main(List<String> arguments) async {
 
   var command = arguments.first;
   switch (command) {
+    // Encrypt the file
     case "clean":
       print("Clean");
       break;
 
+    // Decrypt the file
     case "smudge":
       print("smudge");
       break;
@@ -27,6 +30,9 @@ Future<void> main(List<String> arguments) async {
   }
 }
 
+const _execName = "git-journal-crypt";
+const _version = "0.0.1";
+
 Future<void> init() async {
   var repoPath = GitRepository.findRootDir(Directory.current.path);
   if (repoPath == null) {
@@ -36,29 +42,38 @@ Future<void> init() async {
   }
 
   var repo = await GitRepository.load(repoPath).getOrThrow();
-  var section =
-      repo.config.section('filter').getOrCreateSection("gitjournal-crypt");
+  var section = repo.config
+      .getOrCreateSection('filter')
+      .getOrCreateSection("git-journal-crypt");
 
-  // FIXME: How to check if a section exists?
-  //        Can section names contain spaces?
-  //        Add isEmpty for section
-
-  if (section.options.isNotEmpty) {
+  if (section.isNotEmpty) {
     print(
-        'Error: this repository has already been initialized with git-jcrypt.');
+        'Error: this repository has already been initialized with git-journal-crypt.');
     exit(1);
   }
 
-  section.options['smudge'] = '"git-jcrypt" smudge';
-  section.options['clean'] = '"git-jcrypt" clean';
+  section.options['smudge'] = '"$_execName" smudge';
+  section.options['clean'] = '"$_execName" clean';
 
-  await repo.saveConfig().throwOnError();
+  var gjSection = repo.config.getOrCreateSection(_execName);
+  gjSection.options["version"] = _version;
+  gjSection.options["password"] = _generatePassword();
 
-  print("installed");
-
-  // FIXME: Add "gitjournal-crypt section"
-  // - Add version and password
-  //
+  var r = await repo.saveConfig();
+  if (r.isFailure) {
+    print(r.stackTrace);
+    exit(1);
+  }
 }
 
-// I would like tests for all of this
+// Wouldn't it be better to generate a more secure password and just base64 encode it?
+String _generatePassword() {
+  const _chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+
+  var _rnd = Random.secure();
+  var length = 32;
+
+  return String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+}
